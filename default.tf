a346e890fc16df719c22005c590b762bb5d520ba
Brad Soper <brad.soper@gmail.com>
2018-09-05 09:14:35 -0700

    Added comment lines and cleaned up structure

--- default.tf
+++ default.tf
@@ -1,22 +1,26 @@
 # Version 7
+
+# Defines the type of resource that will be created and the region in AWS the instance will be deployed.
 provider "aws" {
   region = "us-east-1"
 }
 
+# Stores the terraform state file in S3 bucket. 
  terraform {
   backend "s3" {
     bucket = "brad.bucket"
     key    = "terraform.tfstate"
     region = "us-east-1"
-  }
- }
+}
+}
 
-resource "aws_instance" "RadditwithFile" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
-  count = 1
-  key_name   = "Default"
-  security_groups = ["launch-wizard-1"]
+# Defines the AWS instance, provides SSH credentials for access. Uploads configuration.sh to the local /tmp directory.
+ resource "aws_instance" "RadditwithFile" {
+    ami           = "ami-2757f631"
+    instance_type = "t2.micro"
+    count = 1
+    key_name   = "Default"
+    security_groups = ["launch-wizard-1"]
 
   provisioner "file" {
     source      = "configuration.sh"
@@ -25,8 +29,10 @@ resource "aws_instance" "RadditwithFile" {
         type = "ssh"
         user = "ubuntu"
         private_key = "${file("~/.ssh/id_rsa_e7b9b011c0608d0ab820830ab7c0d048")}"
-              }
 }
+}
+
+# Uploads the ubuntu.service file to the /tmp directory. This file is used during when the configuration.sh script is ran.
   provisioner "file" {
     source      = "ubuntu.service"
     destination = "/tmp/ubuntu.service"
@@ -34,8 +40,10 @@ resource "aws_instance" "RadditwithFile" {
         type = "ssh"
         user = "ubuntu"
         private_key = "${file("~/.ssh/id_rsa_e7b9b011c0608d0ab820830ab7c0d048")}"
-              }
 }
+}
+
+# Makes the configuration and ubuntu.service file executable by root accounts. Then the configuration.sh is ran.
   provisioner "remote-exec" {
    inline = [
      "chmod +x /tmp/configuration.sh",
